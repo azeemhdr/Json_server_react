fc5bf432dbf321fb79577b036cd07b532592d0e0
azeemhdr <comazeem@gmail.com>
2024-12-25 12:53:15 +0530

    data posted through axios to json, data created successfully success code 201

--- src/App.js
+++ src/App.js
@@ -1,39 +1,58 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { useState } from 'react';
+import axios from 'axios'
 
 function App() {
  const [name,setName]= useState('')
  const [email,setEmail]= useState('')
- const [age,setAge]= useState('')
- const [image,setImage]= useState(0)
+ const [age,setAge]= useState(0)
+ const [image,setImage]= useState('')
 
  const createUser= (e)=>{
   e.preventDefault()
   fetch('http://localhost:3000/users').then(response=>response.json()).then((data)=>{
+    // console.log(data)
     const id = data.length+1
+    const userData ={id,name,image,email,age}
+    axios.post('http://localhost:3000/users',userData).then(function(response){
+      console.log(response)
+    })
   })
  }
  
   const styles = {
     main: {
-      mainHeight: "100vh",
+      minHeight: "100vh",
+      padding:34
     },
     form:{
       width:500
     },
   };
   return (
-    <div className="main" style={styles.main}>
+    <div className="main " style={styles.main} data-bs-theme="dark">
       <h3>Users</h3>
       <form onSubmit={createUser} style={styles.form}>
-        <input type="text" className="" placeholder="Name" value={name} onChange={(e)=>{setName(e.target.value)}}/>
-        <input type="text" className="" placeholder="Email" value={email} onChange={(e)=>{setEmail(e.target.value)}}/>
-        <input type="text" className="" placeholder="Age" value={age} onChange={(e)=>{setAge(e.target.value)}}/>
-        <input type="text" className="" placeholder="Image" value={image} onChange={(e)=>{setImage(e.target.value)}}/>
-        <button>Create</button>
-
+        <input type="text" className="form-control mb-3" placeholder="Name" value={name} onChange={(e)=>{setName(e.target.value)}}/>
+        <input type="text" className="form-control mb-3" placeholder="Email" value={email} onChange={(e)=>{setEmail(e.target.value)}}/>
+        <input type="text" className="form-control mb-3" placeholder="Age" value={age} onChange={(e)=>{setAge(e.target.value)}}/>
+        <input type="text" className="form-control mb-3" placeholder="Image" value={image} onChange={(e)=>{setImage(e.target.value)}}/>
+        <button className="btn btn-success">Create</button>
       </form>
+      <hr/>
+    <table className="table table-bordered ">
+      <thead>
+        <tr>
+         <th>ID</th>
+          <th>Name</th>
+          <th>Email</th>
+          <th>Age</th>
+          <th>Image</th>
+          <th>Action</th>
+        </tr>
+      </thead>
+    </table>
     </div>
   );
 }
