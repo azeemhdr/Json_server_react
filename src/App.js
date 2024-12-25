bbdd151662196ba7f386443af67d23f86da0bb78
azeemhdr <comazeem@gmail.com>
2024-12-25 20:12:20 +0530

    data show in the table

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import axios from 'axios'
 
 function App() {
  const [name,setName]= useState('')
  const [email,setEmail]= useState('')
  const [age,setAge]= useState(0)
- const [image,setImage]= useState('')
+ const [image,setImage]= useState('https://picsum.photos/200/200')
+ const [users,setUsers] = useState([])
+ const [userData,SetUserData] = useState({})
 
  const createUser= (e)=>{
   e.preventDefault()
   fetch('http://localhost:3000/users').then(response=>response.json()).then((data)=>{
     // console.log(data)
     const id = data.length+1
-    const userData ={id,name,image,email,age}
-    axios.post('http://localhost:3000/users',userData).then(function(response){
-      console.log(response)
+     
+    axios.post('http://localhost:3000/users',{id,name,image,email,age}).then(function(response){
+      // console.log(response)
+      SetUserData({id,name,image,email,age})
+      // fetchUsersData()
     })
   })
  }
+//  const fetchUsersData = ()=>{
+//  axios.get('http://localhost:3000/users').then((response)=>{
+//   SetUserData(response.data)
+//  }) 
+//  }
+ useEffect(()=>{
+  axios.get('http://localhost:3000/users').then((response)=>{
+    setUsers(response.data)
+    // fetchUsersData()
+  })
+ },[userData])
  
   const styles = {
     main: {
@@ -29,6 +44,9 @@ function App() {
     form:{
       width:500
     },
+    image:{
+      width:100
+    }
   };
   return (
     <div className="main " style={styles.main} data-bs-theme="dark">
@@ -48,10 +66,23 @@ function App() {
           <th>Name</th>
           <th>Email</th>
           <th>Age</th>
-          <th>Image</th>
+          <th style={{width:100}}>Image</th>
           <th>Action</th>
         </tr>
       </thead>
+      <tbody>
+        {(users.length) !=0?users.map((item,index)=>{
+          return <tr key={index}>
+            <td>{item.id}</td>
+            <td>{item.name}</td>
+            <td>{item.email}</td>
+            <td>{item.age}</td>
+            <td><img src={item.image} style={styles.image}/></td>
+            <td><button className="btn btn-danger fw-bold">View</button></td>
+
+          </tr>
+        }):'Loading...'}
+      </tbody>
     </table>
     </div>
   );
